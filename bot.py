4d977ae55e40cac83f94698671c0a76a102e6642
unclebobert <jacksonfu777@gmail.com>
2024-12-14 00:02:00 +1100

    switch to pycord

--- bot.py
+++ bot.py
@@ -8,20 +8,45 @@ import requests
 load_dotenv()
 
 intents = discord.Intents(guilds=True, messages=True, message_content=True)
-bot = commands.Bot(intents=intents, command_prefix='+')
+bot = discord.Bot(intents=intents)
 
 
-@bot.hybrid_command()
-async def embed(ctx: commands.Context):
-    async for msg in ctx.channel.history(limit=5):
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    print("------")
+
+
+@bot.slash_command(name='embed', guild_ids=[])
+async def embed(ctx: discord.ApplicationContext):
+    async for msg in ctx.history(limit=5):
         link = msg.content
         if link.startswith('https://vt.tiktok.com/'):
             real = requests.head(link).headers['location']
-            await ctx.send(real, ephemeral=True)
+            await ctx.respond(real, ephemeral=True)
             return
 
-    await ctx.send('No tiktok links found within the last 5 messages', ephemeral=True)
+    await ctx.respond(
+        'No tiktok links found within the last 5 messages',
+        ephemeral=True
+    )
     return
 
-bot.tree.sync()
+
+@bot.slash_command(name='e', guild_ids=[325465792210665474, 1135514799556526171])
+async def embed_special(ctx: discord.ApplicationContext):
+    async for msg in ctx.history(limit=5):
+        link = msg.content
+        if link.startswith('https://vt.tiktok.com/'):
+            real = requests.head(link).headers['location']
+            await ctx.respond(real, ephemeral=True)
+            return
+
+    await ctx.respond(
+        'No tiktok links found within the last 5 messages',
+        ephemeral=True
+    )
+    return
+
+
 bot.run(os.getenv('TOKEN'))
